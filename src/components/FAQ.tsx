59ea07745b7c8c2108727d7e83e847291e37c42e
flashworth <paul@paulmc.xyz>
2026-02-15 18:49:13 +0000

    Updated FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -13,11 +13,12 @@ export default function FAQ({ onClose }: FAQProps) {
       </div>
       <div className="faq__content">
         <div className="faq__item">
-          <h3 className="faq__question">What is this calculator for?</h3>
+          <h3 className="faq__question">What is this for?</h3>
           <p className="faq__answer">
-            Converting your running times and paces into different units on the
-            fly. Add whichever data you have and the calculator will display
-            your duration, pace distance and speed in kilometers and miles.
+            If you're like me, you love checking your running pace against your
+            goals and common benchmarks. That's what this app's for! Give it a
+            pace and a distance, a distance and a time, or a pace and a time,
+            and it'll do the rest.
           </p>
         </div>
 
@@ -26,6 +27,15 @@ export default function FAQ({ onClose }: FAQProps) {
           <p className="faq__answer">
             This app runs completely locally on your device. Nothing is sent
             anywhere.
+          </p>
+        </div>
+        <div className="faq__item">
+          <h3 className="faq__question">What do you mean by "Open"?</h3>
+          <p className="faq__answer">
+            Open Running Calculator is an open source project hosted on{" "}
+            <a href="https://github.com/flashworth/runningcalculator">Github</a>
+            . You can take the code and use it, modify it and share it with
+            others.
           </p>
         </div>
       </div>
